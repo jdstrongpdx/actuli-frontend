af50d1afe03b3b1d89e9af07faeebf8f73540025
jdstrongpdx <jdstrongpdx@gmail.com>
2025-01-14 15:31:11 -0800

    Added files for initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}', // Adjust for your file extensions
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
